3384bcce2c8c64d3c349e38f49fe84d37be687d2
unknown <s4193655@rmit.edu.vn>
2025-10-11 10:51:32 +0700

    add routes for post and delete with auth gaurd

--- backend/src/modules/courses/courses.routes.ts
+++ backend/src/modules/courses/courses.routes.ts
@@ -5,16 +5,24 @@
  */
 import { Router } from "express";
 
+import { authGuard } from "../../middleware/authGuard.js";
 import {
+  deleteCourseStudent,
   getCourse,
   getCourses,
   getCourseStudents,
   postCourse,
+  postCourseStudent,
 } from "./courses.controller.js";
 
 export const courseRouter = Router();
 
+courseRouter.use(authGuard);
+
 courseRouter.get("/", getCourses);
 courseRouter.post("/", postCourse);
 courseRouter.get("/:courseId/students", getCourseStudents);
+courseRouter.post("/:courseId/students", postCourseStudent);
+courseRouter.delete("/:courseId/students/:studentId", deleteCourseStudent);
 courseRouter.get("/:courseId", getCourse);
+
